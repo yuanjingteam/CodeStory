6a5bc5053ae8df2949f67863a15ae393c0172c44
vento <210567852@qq.com>
2026-05-25 20:25:05 +0800

    fix: 解决章节数量过多，用户无法快速定位的问题

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -2,5 +2,7 @@ export { default as SearchFilter } from './SearchFilter';
 export { default as DataTable } from './DataTable';
 export { default as Pagination } from './Pagination';
 export { default as ConfirmDialog } from './ConfirmDialog';
+export { default as SearchableSelect } from './SearchableSelect';
 export type { FilterOption, FilterField } from './SearchFilter';
 export type { Column } from './DataTable';
+export type { SearchableSelectOption } from './SearchableSelect';
