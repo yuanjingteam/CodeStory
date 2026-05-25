f142da37d3890bb216b2c9662e792ac828172810
liushuai-man <2730132473@qq.com>
2026-05-25 21:17:28 +0800

    feat: 修改项目架子的高度样式2

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
         <div className="fixed inset-0 pointer-events-none z-0">
           <div className="absolute inset-0 bg-[radial-gradient(circle,_#000_1px,_transparent_1px)] bg-[size:20px_20px]"></div>
         </div>
-        <div className="w-[95%] h-min-[100vh] max-w-[1500px] mx-auto my-4 border-2 border-black bg-white shadow-[4px_4px_0px_#000] rounded-sm relative z-10">
+        <div className="w-[95%] min-h-[calc(100vh-2rem)] max-w-[1500px] mx-auto my-4 border-2 border-black bg-white shadow-[4px_4px_0px_#000] rounded-sm relative z-10 flex flex-col">
           {children}
         </div>
         <Toaster
