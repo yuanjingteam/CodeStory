2218c66f436b9dc146bb9218d47fb5e30c49eee8
liushuai-man <2730132473@qq.com>
2026-05-25 15:10:21 +0800

    feat: 调整关于样式

--- frontend/src/components/about/AboutDetails.tsx
+++ frontend/src/components/about/AboutDetails.tsx
@@ -25,17 +25,15 @@ export default function AboutHero() {
       "
     >
       <div className="flex justify-center items-center">
-        <div className="flex-3 border-2  border-gray-300 rounded-xl p-4">
-          <div className="flex flex-col justify-between h-full items-center mb-6">
+        <div className="flex-3  rounded-xl py-4">
+          <div className="flex flex-col justify-between h-full items-center mb-10">
             <div className="pr-4">
-              <h1 className="text-4xl font-extrabold mb-4 text-black  flex items-center gap-2">
+              <h1 className="text-4xl font-extrabold mb-6 text-black  flex items-center gap-2">
                 关于 CodeStory <BsRobot className="w-10 h-10 text-purple-600" />
               </h1>
-              <p>
+              <p className="indent-8">
                 CodeStory 是 一个以 AI
-                Agent交互为核心的编程学习平台，致力于重塑传统单向、枯燥且缺乏沉浸感的编程学习方式
-              </p>
-              <p className=" text-gray-700">
+                Agent交互为核心的编程学习平台，致力于重塑传统单向、枯燥且缺乏沉浸感的编程学习方式。
                 我们的目标是通过
                 CodeStory，帮助用户更高效地学习编程，同时提供一个 fun
                 和互动的学习环境。
@@ -43,9 +41,9 @@ export default function AboutHero() {
             </div>
           </div>
           {/* 核心优势三块卡片 */}
-          <div className="flex justify-start items-center gap-4 mb-4">
+          <div className="flex justify-start items-center gap-4 my-4">
             <div className="border-2 rounded-xl border-black shadow-[2px_2px_0_0_rgba(0,0,0,1)] hover:shadow-none hover:translate-[2px] transition-all duration-150 p-5 bg-white">
-              <div className="flex items-center gap-3 mb-2">
+              <div className="flex items-center gap-3 m-2">
                 <div className="w-8 h-8 flex items-center justify-center  rounded-full  text-purple-500">
                   <BsRobot className="w-8 h-8" />
                 </div>
@@ -54,7 +52,7 @@ export default function AboutHero() {
               <p className="text-gray-700">智能 AI Agent 陪伴学习</p>
             </div>
             <div className="border-2 rounded-xl border-black shadow-[2px_2px_0_0_rgba(0,0,0,1)] hover:shadow-none hover:translate-[2px] transition-all duration-150 p-5 bg-white">
-              <div className="flex items-center gap-3 mb-2">
+              <div className="flex items-center gap-3 m-2">
                 <div className="w-8 h-8 flex items-center justify-center  text-green-600">
                   <FiMonitor className="w-8 h-8" />
                 </div>
@@ -63,7 +61,7 @@ export default function AboutHero() {
               <p className="text-gray-700">边学边练，学以致用</p>
             </div>
             <div className="border-2 rounded-xl border-black shadow-[2px_2px_0_0_rgba(0,0,0,1)] hover:shadow-none hover:translate-[2px] transition-all duration-150 p-5 bg-white">
-              <div className="flex items-center gap-3 mb-2">
+              <div className="flex items-center gap-3 m-2">
                 <div className="w-8 h-8 flex items-center justify-center  text-blue-500">
                   <FiTarget className="w-8 h-8" />
                 </div>
@@ -108,6 +106,22 @@ export default function AboutHero() {
           </div>
           <p className="text-gray-800 font-medium text-sm">400-400-4000</p>
           <p className="text-gray-500 text-xs mt-1">电话</p>
+        </div>
+        {/* 电话 */}
+        <div className="flex-1 flex flex-col items-center justify-center p-4 border-2 border-gray-500 rounded-lg bg-white ">
+          <div className="w-12 h-12 flex items-center justify-center bg-blue-500 rounded-full border-2 border-blue-500 mb-2">
+            <FiPhone className="w-6 h-6 text-white" />
+          </div>
+          <p className="text-gray-800 font-medium text-sm">400-400-4000</p>
+          <p className="text-gray-500 text-xs mt-1">微信号</p>
+        </div>
+        {/* 电话 */}
+        <div className="flex-1 flex flex-col items-center justify-center p-4 border-2 border-gray-500 rounded-lg bg-white ">
+          <div className="w-12 h-12 flex items-center justify-center bg-blue-500 rounded-full border-2 border-blue-500 mb-2">
+            <FiPhone className="w-6 h-6 text-white" />
+          </div>
+          <p className="text-gray-800 font-medium text-sm">400-400-4000</p>
+          <p className="text-gray-500 text-xs mt-1">公众号</p>
         </div>
       </div>
       {/* 用户口碑卡片 */}
